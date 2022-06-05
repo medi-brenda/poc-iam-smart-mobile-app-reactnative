2e3fcb205ecf08b2db82f334b909da9c518b9d06
unknown <brenda_hon@mediconcen.com>
2022-06-05 10:47:22 +0800

    url

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Text, Button, View, StyleSheet } from 'react-native';
-import * as Linking from 'expo-linking';
+import { Text, Button, View, StyleSheet, Linking } from 'react-native';
+import * as ExpoLinking from 'expo-linking';
 import * as WebBrowser from 'expo-web-browser';
 import Constants from 'expo-constants';
 
@@ -12,13 +12,13 @@ export default class App extends Component {
         <Text>In poc-iam-smart-mobile-app-reactnative</Text>
 
         <Button
-          title="Open Online Service App with ReactNative.Linking Expo"
+          title="Open Online Service App with Expo ReactNative.Linking Expo"
           onPress={this._handleOpenWithLinkingExpo}
           style={styles.button}
         />
 
         <Button
-          title="Open Online Service App with ReactNative.Linking EAS"
+          title="Open Online Service App with Expo ReactNative.Linking EAS"
           onPress={this._handleOpenWithLinkingEAS}
           style={styles.button}
         />
@@ -39,20 +39,26 @@ export default class App extends Component {
     );
   }
 
+  _expoAppURL = "exp://exp.host/@medibrenda/poc-iam-smart-onlineservice-app-reactnative?release-channel=default";
+  _easAppURL = "exp+poc-iam-smart-onlineservice-app-reactnative://expo-development-client/?url=https%3A%2F%2Fu.expo.dev%2F4b77fc4f-ea89-452e-b491-d103e3cac86c%3Fchannel-name%3Dmain";
+
+  _expoRedirectURL = "https://mediconcen.com/poc-iam-smart/poc-iam-smart-html/call-poc-iam-smart-onlineservice-app-reactnative_expo.html";
+  _easRedirectURL = "https://mediconcen.com/poc-iam-smart/poc-iam-smart-html/call-poc-iam-smart-onlineservice-app-reactnative_eas.html";
+
   _handleOpenWithLinkingExpo = () => {
-    Linking.openURL('exp://exp.host/@medibrenda/poc-iam-smart-onlineservice-app-reactnative?release-channel=default');
+    ExpoLinking.openURL(this._expoAppURL);
   };
 
   _handleOpenWithLinkingEAS = () => {
-    Linking.openURL('exp+poc-iam-smart-onlineservice-app-reactnative://expo-development-client/?url=https%3A%2F%2Fu.expo.dev%2F4b77fc4f-ea89-452e-b491-d103e3cac86c%3Fchannel-name%3Dmain');
+    ExpoLinking.openURL(this._easAppURL);
   };
 
   _handleOpenWithWebBrowserExpo = () => {
-    WebBrowser.openBrowserAsync('https://mediconcen.com/poc-iam-smart/poc-iam-smart-html/call-poc-iam-smart-onlineservice-app-reactnative_expo.html');
+    WebBrowser.openBrowserAsync(this._expoRedirectURL);
   };
 
   _handleOpenWithWebBrowserEAS = () => {
-    WebBrowser.openBrowserAsync('https://mediconcen.com/poc-iam-smart/poc-iam-smart-html/call-poc-iam-smart-onlineservice-app-reactnative_eas.html');
+    WebBrowser.openBrowserAsync(this._easRedirectURL);
   };
 }
 
