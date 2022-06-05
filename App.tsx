ce7fa59bcc76d946776015ca0275bd575fcd9932
unknown <brenda_hon@mediconcen.com>
2022-06-05 09:07:00 +0800

    eas

--- App.tsx
+++ App.tsx
@@ -12,25 +12,47 @@ export default class App extends Component {
         <Text>In poc-iam-smart-mobile-app-reactnative</Text>
 
         <Button
-          title="Open Online Service App with ReactNative.Linking"
-          onPress={this._handleOpenWithLinking}
+          title="Open Online Service App with ReactNative.Linking Expo"
+          onPress={this._handleOpenWithLinkingExpo}
           style={styles.button}
         />
+
         <Button
-          title="Open Online Service App with Expo.WebBrowser"
-          onPress={this._handleOpenWithWebBrowser}
+          title="Open Online Service App with ReactNative.Linking EAS"
+          onPress={this._handleOpenWithLinkingEAS}
           style={styles.button}
         />
+
+        <Button
+          title="Open Online Service App with Expo.WebBrowser Expo"
+          onPress={this._handleOpenWithWebBrowserExpo}
+          style={styles.button}
+        />
+
+        <Button
+          title="Open Online Service App with Expo.WebBrowser EAS"
+          onPress={this._handleOpenWithWebBrowserEAS}
+          style={styles.button}
+        />
+
       </View>
     );
   }
 
-  _handleOpenWithLinking = () => {
+  _handleOpenWithLinkingExpo = () => {
     Linking.openURL('exp://exp.host/@medibrenda/poc-iam-smart-onlineservice-app-reactnative?release-channel=default');
   };
 
-  _handleOpenWithWebBrowser = () => {
-    WebBrowser.openBrowserAsync('https://mediconcen.com/poc-iam-smart/poc-iam-smart-html/call-poc-iam-smart-onlineservice-app-reactnative.html');
+  _handleOpenWithLinkingEAS = () => {
+    Linking.openURL('exp+poc-iam-smart-onlineservice-app-reactnative://expo-development-client/?url=https%3A%2F%2Fu.expo.dev%2F4b77fc4f-ea89-452e-b491-d103e3cac86c%3Fchannel-name%3Dmain');
+  };
+
+  _handleOpenWithWebBrowserExpo = () => {
+    WebBrowser.openBrowserAsync('https://mediconcen.com/poc-iam-smart/poc-iam-smart-html/call-poc-iam-smart-onlineservice-app-reactnative_exp.html');
+  };
+
+  _handleOpenWithWebBrowserEAS = () => {
+    WebBrowser.openBrowserAsync('https://mediconcen.com/poc-iam-smart/poc-iam-smart-html/call-poc-iam-smart-onlineservice-app-reactnative_eas.html');
   };
 }
 
